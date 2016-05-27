b4526ec57b265d40375d113fb16d3f6180f7e06f
jonnorstrom <jon.norstrom@gmail.com>
2016-05-27 10:40:18 -0500

    removed extra dependencies

--- rhonda.rb
+++ rhonda.rb
@@ -1,18 +1,15 @@
 require 'sinatra'
 require_relative './public/badge'
 require_relative './public/parser'
-require 'json'
 
 get '/' do
- p "hello world"
+
 end
 
 post '/badge' do
   # made badge in database that I don't have set up yet
   message_from_slack = params[:text]
   badge = Badge.new(Parser.get_badge_data(message_from_slack))
-  # content_type :json
-  # {text: "You think #{badge.person} is pretty great"}.to_json
   "Thank you for your feedback! You gave #{badge.person} some #{badge.type} #{badge.note}"
 end
 
