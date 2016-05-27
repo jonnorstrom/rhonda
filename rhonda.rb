a90c30f84fbef89ee4ccc8ec21ff8a909b129a38
jonnorstrom <jon.norstrom@gmail.com>
2016-05-27 03:28:21 -0500

    can I send JSON back to SLACK?

--- rhonda.rb
+++ rhonda.rb
@@ -9,7 +9,14 @@ end
 
 post '/badge' do
   # made badge in database that I don't have set up yet
-  params[:text]
+  {
+    "text": "#{params[:text]}",
+    "attachments": [
+      {
+        "text":"And you gave him a star"
+      }
+    ]
+  }
 end
 
 
