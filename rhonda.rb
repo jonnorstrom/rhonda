48f328ce8251da9b60c950afa4a75e8857e33796
jonnorstrom <jon.norstrom@gmail.com>
2016-05-27 10:01:16 -0500

    Testing all aspects of the badge now

--- rhonda.rb
+++ rhonda.rb
@@ -13,28 +13,8 @@ post '/badge' do
   badge = Badge.new(Parser.get_badge_data(message_from_slack))
   # content_type :json
   # {text: "You think #{badge.person} is pretty great"}.to_json
-  badge.to_json
+  "Thank you for your feedback! You gave #{badge.person} some #{badge.type} #{badge.note}"
 end
-
-post '/gateway' do
-  message = params[:text].gsub(params[:trigger_word], '').strip
-
-  action, repo = message.split('_').map {|c| c.strip.downcase }
-  repo_url = "https://api.github.com/repos/#{repo}"
-
-  case action
-    when 'issues'
-      resp = HTTParty.get(repo_url)
-      resp = JSON.parse resp.body
-      respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
-  end
-end
-
-def respond_message message
-  content_type :json
-  {:text => message}.to_json
-end
-
 
 # POTENTIAL IDEAL WORK FLOW
 ###########################################################
