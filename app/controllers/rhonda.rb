b9ed9a3e23d8a0f2feb5b42e6c487e16c0c906db
jonnorstrom <jon.norstrom@gmail.com>
2016-06-01 13:29:18 -0500

    testing if/else statements

--- app/controllers/rhonda.rb
+++ app/controllers/rhonda.rb
@@ -4,22 +4,23 @@ end
 
 post '/badge' do
   message_from_slack = params[:text]
-  info_hash = Parser.get_badge_data(message_from_slack)
-  badge = Feedback.new(recipient: info_hash[:recipient],
-                       sender: params[:user_name],
-                       sender_id: params[:user_id],
-                       team_domain: params[:team_domain],
-                       team_id: params[:team_id],
-                       channel_name: params[:channel_name],
-                       channel_id: params[:channel_id],
-                       response_url: params[:response_url],
-                       text: params[:text],
-                       quantity: info_hash[:quantity],
-                       reason: info_hash[:reason],
-                       badge: info_hash[:badge])
+  if info_hash = Parser.get_badge_data(message_from_slack)
+    badge = Feedback.new(recipient: info_hash[:recipient],
+                         sender: params[:user_name],
+                         sender_id: params[:user_id],
+                         team_domain: params[:team_domain],
+                         team_id: params[:team_id],
+                         channel_name: params[:channel_name],
+                         channel_id: params[:channel_id],
+                         response_url: params[:response_url],
+                         text: params[:text],
+                         quantity: info_hash[:quantity],
+                         reason: info_hash[:reason],
+                         badge: info_hash[:badge])
+  end
 
   if badge.save
-    "Thank you for your feedback! You gave #{badge.person} some #{badge.type} #{badge.note}"
+    "Thank you for your feedback! You gave #{badge.recipient} some #{badge.badge} #{badge.reason}"
   else
     "Something went wrong, try this format: [#] [badge] to @[person] for [reason]"
   end
