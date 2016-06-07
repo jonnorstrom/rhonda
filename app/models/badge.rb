43faa0be36a5aa076ca3e9ea57ec47134df3575b
jonnorstrom <jon.norstrom@gmail.com>
2016-06-07 18:33:39 -0500

    deleted badge.rb file

--- app/models/badge.rb
+++
@@ -1,9 +0,0 @@
-# class Badge
-#   attr_reader :quantity, :type, :person, :notes
-#
-#   def initialize(hash)
-#     @type = hash[:type]
-#     @person = hash[:person]
-#     @notes = hash[:note]
-#   end
-# end
