e9189bcf895c0d921ebec76a6db3aaedff9a2bc9
jonnorstrom <jon.norstrom@gmail.com>
2016-06-07 14:38:32 -0500

    looking at member names

--- app/controllers/rhonda.rb
+++ app/controllers/rhonda.rb
@@ -23,7 +23,7 @@ post '/badge' do
   user_response = JSON.parse(response.body)
   p "BADGE RECIPIENT -- #{badge.recipient}"
   user_response["members"].each do |member|
-  p "MEMBER -- #{member.name}"
+  p "MEMBER -- #{member["name"]}"
     if member[:name] == badge.recipient
       # badge.recipient_id = member[:id]
       p "HEY THEY MATCH"
