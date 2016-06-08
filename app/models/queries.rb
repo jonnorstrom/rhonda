c7986b7771c680b6f4fed006827d1637feae39a2
jonnorstrom <jon.norstrom@gmail.com>
2016-06-08 10:09:25 -0500

    added some helper query methods

--- app/models/queries.rb
+++ app/models/queries.rb
@@ -1,24 +1,38 @@
 module Queries
-  def self.get_all_badges(badge_type, user_name)
-    Feedback.where("badge = ? AND recipient = ?", badge_type, user_name)
+  ## gets all badges according to the three params - badge_type and user_name, and recipient/sender
+  ## Queries.get_all_badges("stars", "trev", "recipient")
+  def self.get_all_badges(badge_type, user_name, position)
+    Feedback.where("badge = ? AND #{position} = ?", badge_type, user_name)
   end
 
+  ## returns an integer that is the sum of the quantity field of a collection of badges
+  ## Queries.add_all_quantities(Queries.get_all_badges("stars", "trev", "recipient"))
   def self.add_all_quantities(collection)
     collection.map { |badge| badge.quantity }.reduce(:+)
   end
 
+  ## returns and array of hashes that include the username and how many Feedack object's they've created in the database. Pay no attention to parameters
+  ## Queries.find_most_active(Queries.find_all_participants)
   def self.find_most_active(collection)
     total = 0
-    most_active = collection.keep_if {| key, value | value >= total }
+    most_active = []
+    collection.each do | key, value |
+      if value >= total
+        total = value
+        most_active.clear << {key => value}
+      end
+    end
+    most_active
   end
 
+  ## gathers a hash of all the participants and how many times they've created a Feedback object
   def self.find_all_participants
     collection = {}
     Feedback.all.each do |badge|
       if collection.has_key?(badge.sender)
-        collection[sender] += 1
+        collection[badge.sender] += 1
       else
-        collection[sender] = 1
+        collection[badge.sender] = 1
       end
     end
     collection
